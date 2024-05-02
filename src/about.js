3de0e855b0ef1d7ea0dde197e76249724a3146f0
paola4 <arevalo@ualberta.ca>
2024-05-02 16:07:50 -0600

    Add content, update styles

--- src/about.js
+++ src/about.js
@@ -1,9 +1,41 @@
 function about() {
-  const element = document.createElement("div");
-
-  element.textContent = "about";
   console.log("about tab loaded");
+  const aboutWrapper = document.createElement("div");
+  aboutWrapper.classList.add("about-wrapper");
+  const header = document.createElement("h1");
+  header.innerHTML = "la once";
+  aboutWrapper.appendChild(header);
 
-  return element;
+  const intro = document.createElement("p");
+  intro.innerHTML = `Welcome to <b>la once</b>, where the charm of Chilean tradition meets the warmth of a family gathering. Nestled in the heart of the community, our restaurant invites you to relive the cherished custom of <b>"once"</b>, Chile’s beloved afternoon tea time. At la once, every dish tells a story and every sip brings a smile.`;
+  aboutWrapper.appendChild(intro);
+
+  const aboutContent = {
+    "Our Story":
+      "la once was born from a simple dream: to bring people together just as families in Chile gather in the late afternoon to share tea, freshly baked breads, and delightful pastries. Inspired by this century-old tradition, our founders, siblings Ana and José Rivera, transformed their family’s recipes into a culinary experience for all to enjoy. Our doors opened with the hope of not only serving food but also fostering connections.",
+    "Our Cuisine":
+      "We pride ourselves on authenticity and quality, crafting each dish using traditional ingredients and methods passed down through generations. From the buttery softness of our homemade pan amasado to the sweet and tantalizing layers of our milhojas, each bite is a nod to Chilean culinary artistry. Our menu features an array of options from savory empanadas to sweet kuchen, accompanied by a selection of the finest teas and the robust flavors of Chilean coffee.",
+    "Our Space":
+      "la once is designed to feel like a home. The decor blends rustic touches with contemporary comfort, creating a cozy yet elegant atmosphere. Whether you’re nestled by the fireplace in the winter or enjoying the breeze on our patio in the summer, our space is perfect for relaxing and making memories.",
+    "Join Us":
+      "We invite you to step into la once, not just to eat, but to take part in a tradition that spans generations. Join us in the afternoon for a leisurely tea, bring your family for a special weekend treat, or celebrate your special moments with us. At la once, you’re always among friends.",
+  };
+
+  for (const [title, content] of Object.entries(aboutContent)) {
+    const section = document.createElement("div");
+    section.classList.add("about-section");
+
+    const sectionTitle = document.createElement("h2");
+    sectionTitle.innerHTML = title;
+    section.appendChild(sectionTitle);
+
+    const sectionContent = document.createElement("p");
+    sectionContent.innerHTML = content;
+    section.appendChild(sectionContent);
+
+    aboutWrapper.appendChild(section);
+  }
+
+  return aboutWrapper;
 }
 export default about;
--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,95 @@
 function menu() {
-  const element = document.createElement("div");
-
-  element.textContent = "Menu";
   console.log("Menu tab loaded");
+  const menuWrapper = document.createElement("div");
+  menuWrapper.classList.add("tab-wrapper", "menu-wrapper");
 
-  return element;
+  // Create drinks card
+  const drinks = document.createElement("div");
+  drinks.classList.add("menu-card");
+  drinks.classList.add("drinks");
+  const drinksTitle = document.createElement("div");
+  drinksTitle.classList.add("menu-title-wrapper");
+  drinksTitle.innerHTML = `<h2>Drinks</h2>
+                           <i class="ph-thin ph-coffee"></i>`;
+  drinks.appendChild(drinksTitle);
+  const drinksOptions = document.createElement("div");
+  drinksOptions.classList.add("menu-options");
+  const drinksObject = {
+    Espresso: "1.50",
+    Cappuccino: "3.00",
+    Latte: "3.50",
+    Macchiato: "2.50",
+    Mocha: "4.00",
+    Americano: "2.00",
+    "Flat White": "3.00",
+    "Hot Chocolate": "2.50",
+    Tea: "1.50",
+  };
+
+  for (const [drink, price] of Object.entries(drinksObject)) {
+    const drinkOption = document.createElement("div");
+    drinkOption.classList.add("menu-option");
+    drinkOption.innerHTML = `<p>${drink}</p><p>${price}</p>`;
+    drinksOptions.appendChild(drinkOption);
+  }
+  drinks.appendChild(drinksOptions);
+  menuWrapper.appendChild(drinks);
+
+  // Create breads card
+  const breads = document.createElement("div");
+  breads.classList.add("menu-card");
+  breads.classList.add("breads");
+  const breadsTitle = document.createElement("div");
+  breadsTitle.classList.add("menu-title-wrapper");
+  breadsTitle.innerHTML = `<h2>Breads</h2>
+                            <i class="ph-thin ph-bread"></i>`;
+  breads.appendChild(breadsTitle);
+  const breadsOptions = document.createElement("div");
+  breadsOptions.classList.add("menu-options");
+  const breadsObject = {
+    Amasado: "1.50",
+    Marraqueta: "3.00",
+    Hallula: "3.50",
+    Coliza: "2.50",
+    Sopaipilla: "4.00",
+  };
+
+  for (const [bread, price] of Object.entries(breadsObject)) {
+    const breadOption = document.createElement("div");
+    breadOption.classList.add("menu-option");
+    breadOption.innerHTML = `<p>${bread}</p><p>${price}</p>`;
+    breadsOptions.appendChild(breadOption);
+  }
+  breads.appendChild(breadsOptions);
+  menuWrapper.appendChild(breads);
+
+  // Create spreads card
+  const spreads = document.createElement("div");
+  spreads.classList.add("menu-card");
+  spreads.classList.add("spreads");
+  const spreadsTitle = document.createElement("div");
+  spreadsTitle.classList.add("menu-title-wrapper");
+  spreadsTitle.innerHTML = `<h2>Spreads</h2>
+                            <i class="ph-thin ph-knife"></i>`;
+  spreads.appendChild(spreadsTitle);
+  const spreadsOptions = document.createElement("div");
+  spreadsOptions.classList.add("menu-options");
+  const spreadsObject = {
+    Manjar: "1.50",
+    Palta: "3.00",
+    Mermelada: "3.50",
+    "Dulce de membrillo": "2.50",
+  };
+
+  for (const [spread, price] of Object.entries(spreadsObject)) {
+    const spreadOption = document.createElement("div");
+    spreadOption.classList.add("menu-option");
+    spreadOption.innerHTML = `<p>${spread}</p><p>${price}</p>`;
+    spreadsOptions.appendChild(spreadOption);
+  }
+  spreads.appendChild(spreadsOptions);
+  menuWrapper.appendChild(spreads);
+
+  return menuWrapper;
 }
 export default menu;
